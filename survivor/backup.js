7da9f3d4474361b83c3b11fcb30a519a83d15ac2
jsochurek <joe.sochurek@heronsystems.com>
2016-12-08 08:40:43 -0500

    just some stuff with teams

--- survivor/backup.js
+++ survivor/backup.js
@@ -5,51 +5,43 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View,
-  ScrollView
+  View
 } from 'react-native';
 var XMLParser = require('react-xml-parser');
-import {teams} from "./2016/teams";
+
 //let xmlText = require('c:/projects/survivor/survivor/2016/teams2016.xml');
 
 
 export default class survivor extends Component {
-  teams2016;
   constructor() {
     super();
-//     var xmlText = "<?xml version='1.0' encoding='utf-8'?>\
-// <Library>\
-//    <Books count='1'>\
-//        <Book id='1'>\
-//            <Name>Me Before You</Name>\
-//            <Author>Jojo Moyes</Author>\
-//        </Book>\
-//    </Books>\
-//    <Music count=1>\
-//        <CD id='2'>\
-//            <Name>Houses of the Holy</Name>\
-//            <Artist>Led Zeppelin</Artist>\
-//        </CD>\
-//    </Music>\
-// </Library>"
-    this.teams2016 = teams;
-    console.log(this.teams2016);
+    var xmlText = "<?xml version='1.0' encoding='utf-8'?>\
+<Library>\
+   <Books count='1'>\
+       <Book id='1'>\
+           <Name>Me Before You</Name>\
+           <Author>Jojo Moyes</Author>\
+       </Book>\
+   </Books>\
+   <Music count=1>\
+       <CD id='2'>\
+           <Name>Houses of the Holy</Name>\
+           <Artist>Led Zeppelin</Artist>\
+       </CD>\
+   </Music>\
+</Library>"
+    console.log(xmlText);
     //need to define xmlText
-    // var xml = new XMLParser().parseFromString(teams);    // Assume xmlText contains the example XML
-    // console.log(xml);
-    // console.log(xml.getElementsByTagName('Name'));
+    var xml = new XMLParser().parseFromString(xmlText);    // Assume xmlText contains the example XML
+    console.log(xml);
+    console.log(xml.getElementsByTagName('Name'));
   }
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView>
-          {this.teams2016.map((item, index) => {
-            return(
-            <Text key={index}>{item.name}</Text>
-            );
-            })
-          }
-        </ScrollView>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
         </Text>

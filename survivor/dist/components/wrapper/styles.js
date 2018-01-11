e88f840d829689fce9f7d0ffc07cb495989f8d81
jsochurek <joe.sochurek@heronsystems.com>
2018-01-10 21:16:33 -0500

    moving to root, updating pacakges

--- survivor/dist/components/wrapper/styles.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="../../../typings/index.d.ts" />
-import { StyleSheet } from "react-native";
-const styles = StyleSheet.create({
-    wrapper: {
-        flex: 1
-    }
-});
-export default styles;
-//# sourceMappingURL=styles.js.map
